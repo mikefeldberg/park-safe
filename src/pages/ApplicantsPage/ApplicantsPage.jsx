a75aaf97528542ce2beb31c6053dd640c59db8ac
mikefeldberg <mikefeldberg@gmail.com>
2019-08-22 11:22:00 -0700

    typo

--- src/pages/ApplicantsPage/ApplicantsPage.jsx
+++ src/pages/ApplicantsPage/ApplicantsPage.jsx
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
-import { getAllApplicants } from '../../services/api';
-import { CSVLink } from "react-csv";
+import { CSVLink } from 'react-csv';
 
-const csvData =[
-    ['firstname', 'lastname', 'email'] ,
-    ['John', 'Doe' , 'john.doe@xyz.com'] ,
-    ['Jane', 'Doe' , 'jane.doe@xyz.com']
-  ];
-
+const csvData = [
+    ['firstname', 'lastname', 'email'],
+    ['John', 'Doe', 'john.doe@xyz.com'],
+    ['Jane', 'Doe', 'jane.doe@xyz.com'],
+];
 
 class ApplicantsPage extends Component {
-
-    render(){
-        return(
+    render() {
+        return (
             <div>
-            <CSVLink data={csvData} >Download Applications</CSVLink>
+                <CSVLink data={csvData}>Download Applications</CSVLink>
             </div>
-        )
+        );
     }
-
 }
 export default ApplicantsPage;
