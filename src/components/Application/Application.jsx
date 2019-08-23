120cf9e89f5710b4b491c9f7c3689f45b1b20550
mikefeldberg <mikefeldberg@gmail.com>
2019-08-23 01:11:40 -0700

    update list view

--- src/components/Application/Application.jsx
+++ src/components/Application/Application.jsx
@@ -31,42 +31,7 @@ class Application extends Component {
 
     render() {
         return (
-            <div className="container">
-
-                
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
-                    </tbody>
-                </table>
-
-                
-
+            <div className="container topBorder">
                 <div className="row">
                     <div className="col-3">
                         <div>
